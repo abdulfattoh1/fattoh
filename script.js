bdb968a4dfdb2cb464a864a001734141e63a092e
abdulfattoh1 <76897276+abdulfattoh1@users.noreply.github.com>
2021-01-03 12:29:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+var str =prompt('write name');
+console.log(str);
+
+
+var year=prompt('write year');
+console.log(year);
+console.log(str + '='+ year+'year')
+
+var num1 = +prompt('decide example: 13 + 7');
+var num_1 = 20;
+var num = +prompt('decide example: 30-7');
+var num_2 = 23;
+var num3 = +prompt('decide example: 5*8');
+var num_3 = 40;
+var num4 = +prompt('decide example: 90/10');
+var num_4 = 9;
+var num5 = +prompt('decide example: 10%4');
+var num_5 = 2.2;
+console.log('13+ 7 = ' + num_1 + ' ' + '(your answer: ' + num1 + ')');
+console.log(' 30-7='+num_2+'(your answer'+num_2+')');
+console.log('5*8='+num_3+'(your answer'+num3+')');
+console.log('90/10='+num_4+'(your answer'+num4+')');
+console.log('10%4='+num_5+'(your answer'+num5+')');
+
+
